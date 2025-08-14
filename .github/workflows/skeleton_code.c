2696bca13541f09b0a3b2a040050b113a0f5a313
Bhargav-1982 <bhargav1982.github@gmail.com>
2025-08-14 19:27:43 -0400

    Create skeleton_code.c

---
+++ .github/workflows/skeleton_code.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/*
+ * Assignment 1: Basic Input/Output Operations
+ * 
+ * Instructions:
+ * 1. Read two integers from standard input
+ * 2. Calculate and output their sum, difference, and product
+ * 3. Make sure your output format matches the examples exactly
+ */
+
+int main() {
+    // TODO: Declare your variables here
+    
+    
+    // TODO: Read two integers from input
+    
+    
+    // TODO: Calculate sum, difference, and product
+    
+    
+    // TODO: Output the results (one per line)
+    
+    
+    return 0;
+}
